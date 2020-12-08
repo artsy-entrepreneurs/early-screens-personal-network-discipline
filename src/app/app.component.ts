a1d9dfc05f72bdc587a874396d5febf530a50dad
Sumeet Pareek <positivecharge@gmail.com>
2020-12-08 13:09:13 -0500

    Edits title and removes placeholder content (#5)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'early-screens-personal-network-yoga-discipline';
+  title = 'DEMO Personal Network Yoga Discipline';
 }
